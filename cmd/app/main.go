d84ba78f69522ee046c7ee6568228cff3fd710e5
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2025-03-19 20:55:07 +0300

    wip http server for files

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,66 +1,114 @@
 package main
 
 import (
+	"elib/internal/client"
+	"elib/internal/view"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
+	"log/slog"
+	"net/http"
 	"os"
 	"strconv"
-
-	"golang.org/x/net/html"
 )
 
-func GetPageCnt(r io.Reader) (int, error) {
-	doc, err := html.Parse(r)
+func sendFile(w http.ResponseWriter, f *os.File) error {
+	fileInfo, err := f.Stat()
 	if err != nil {
-		return 0, err
+		return errors.New("Error getting file info: " + err.Error())
 	}
-	var lastCnt int
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "option" {
-			lastCnt, err = strconv.Atoi(n.Attr[0].Val)
-			if err != nil {
-				return
-			}
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
+
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileInfo.Name()))
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", fileInfo.Size()))
+
+	_, err = io.Copy(w, f)
+	if err != nil {
+		return errors.New("Error sending file:" + err.Error())
 	}
-	f(doc)
-	return lastCnt, nil
+	return nil
 }
 
 func main() {
-
-	file, err := os.Open("index.html")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	doc, err := html.Parse(file)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	var lastCnt int
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "select" {
-			fmt.Println(n.Data) // print tag name
+	port := flag.String("p", "8080", "http server port")
+	flag.Parse()
+	if p, err := strconv.Atoi(*port); err != nil || p < 0 || p > 65000 {
+		if p < 0 || p > 65000 {
+			panic("invalid http port")
+		} else {
+			panic(err)
 		}
-		if n.Type == html.ElementNode && n.Data == "option" {
-			lastCnt, err = strconv.Atoi(n.Attr[0].Val)
+	}
+
+	if len(os.Args) < 0 {
+		fmt.Println("Usage: app <login> <password>")
+		return
+	}
+	loginRaw := os.Args[1]
+	login, err := strconv.Atoi(loginRaw)
+	if err != nil {
+		panic(err)
+	}
+	pass := os.Args[2]
+
+	elib := client.NewElibClient()
+
+	if err = elib.Login(login, pass); err != nil {
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFileFS(w, r, view.Pages, "index.html")
+	})
+
+	http.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		rawBookID := r.URL.Query().Get("bookId")
+		bookID, err := strconv.Atoi(rawBookID)
+		if err != nil {
+			_, _ = w.Write([]byte("invalid book id"))
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		// checking if book already downloaded
+		filename := fmt.Sprintf("book_%d.pdf", bookID)
+		f, err := os.Open(filename)
+		if err == nil {
+			defer f.Close()
+			err = sendFile(w, f)
 			if err != nil {
-				fmt.Println(err)
-				return
+				slog.Error("error sending cached file:", "err", err.Error())
 			}
+
+			return
 		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+
+		err = elib.DownloadDocument(bookID)
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
+
+		f, err = os.Open(filename)
+		if err != nil {
+			_, _ = w.Write([]byte("unable to open result file: " + err.Error()))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		err = sendFile(w, f)
+		if err != nil {
+			slog.Error("failed to send file", "err", err.Error())
+		}
+	})
+	slog.Info("starting http server on:", "port", *port)
+	if err := http.ListenAndServe(":"+*port, nil); err != nil {
+		panic(err)
 	}
-	f(doc)
-	fmt.Println(lastCnt)
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,18 @@
 module elib
 
-go 1.21.3
+go 1.22
+
+toolchain go1.23.4
 
 require (
+	github.com/gen2brain/go-fitz v1.24.14
 	github.com/jung-kurt/gofpdf v1.16.2
 	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/otiai10/gosseract/v2 v2.4.1
 	golang.org/x/net v0.21.0
+)
+
+require (
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/jupiterrider/ffi v0.2.0 // indirect
 )
